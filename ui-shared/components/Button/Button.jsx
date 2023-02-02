8ac74cf21ddc3c9974dd6376e693d856645b2d62
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2023-02-03 03:26:19 +0500

    refactor: improve building

--- ui-shared/components/Button/Button.jsx
+++ ui-shared/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import { useMemo } from "react";
 import { v4 as uuiv4 } from "uuid";
 
-export const Button6 = ({ type, children }) => {
+export const Button = ({ type, children }) => {
   const id = useMemo(() => uuiv4(), []);
 
   return <button type={type}>{children}</button>;
